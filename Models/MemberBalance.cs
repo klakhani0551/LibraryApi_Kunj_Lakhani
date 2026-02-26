e98c9fbfb573ace23ea02820abda7ae59d2f9d9e
klakhani0551 <klakhani0551@conestogac.on.ca>
2026-02-25 19:01:25 -0500

    Added transactional transfer endpoint

--- Models/MemberBalance.cs
+++ Models/MemberBalance.cs
@@ -2,6 +2,6 @@ namespace LibraryApi.Models;
 
 public class MemberBalance
 {
-    public int id {get; set; }
+    public int Id {get; set; }
     public int Balance {get; set;  }
 }
--- Program.cs
+++ Program.cs
@@ -4,31 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Description = "API Key needed to access endpoints",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Name = "X-Api-Key",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
-    });
-
-    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "ApiKey"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
